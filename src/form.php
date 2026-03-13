511e4df1660bc2b04931b567004327d8617de795
yuuya sinjyou <yuuya20000616@gmail.com>
2026-03-13 20:47:38 +0900

    linkタグのrel属性のタイポを修正

--- src/form.php
+++ src/form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>フォーム入力</title>
-    <link rel="stylecheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
